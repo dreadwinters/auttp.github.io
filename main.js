9b11629c44e61e935f6bd1c3719fb6d1f54e9702
AUTTP <160621237+AUTTP@users.noreply.github.com>
2024-02-20 21:22:56 +0200

    unfuck the js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const audioSources = ["1.mp3", "2.mp3"];
-const player = document.getElementById("player");
-function playAudio() {
-    let audioSource = audioSources[Math.floor(Math.random() * audioSources.length)];
-    player.src = audioSource;
-};
-player.addEventListener('ended', playAudio);
-playAudio();
+document.addEventListener('DOMContentLoaded', function () {
+    const audioSources = ["1.mp3", "2.mp3"];
+    const player = document.getElementById("player");
+
+    function playAudio() {
+        let audioSource = audioSources[Math.floor(Math.random() * audioSources.length)];
+        player.src = audioSource;
+    }
+
+    player.addEventListener('ended', playAudio);
+    playAudio();
+});
