37ca68a2c552c7f23d59de908e3969a6b4713d54
AUTTP <160621237+AUTTP@users.noreply.github.com>
2024-03-05 17:25:56 +0200

    i hate javascript

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const audioSources = ["1.mp3", "2.mp3"];
-    const player = document.getElementById("player");
-
-    function playAudio() {
-        let audioSource = audioSources[Math.floor(Math.random() * audioSources.length)];
-        player.src = audioSource;
-    }
-
-    player.addEventListener('ended', playAudio);
-    playAudio();
-});
+console.log("remind me to add more shit - avek0");
